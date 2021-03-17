75862f98327f225ae5940187dcad8aeb74f2e69e
brun0fz <brunofabrizio15@gmail.com>
2021-03-17 19:51:00 -0300

    update

--- Introduccion/src/com/company/Main.java
+++ Introduccion/src/com/company/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int N=5;
+        double A=3.5;
+        char C='a';
 
-	// write your code here
+        System.out.println("El valor es: "+N+" "+A+" "+C+" ");
+        System.out.println(N+A);
+        System.out.println(A-N);
+        System.out.println(C);
+
+        int valorNumerico = Character.getNumericValue(C);
+
+        System.out.println("El valor numerico de C es: "+valorNumerico);
+
     }
 }

c4a147fa0bc92066abab973d9d94d0efc76285da
brun0fz <brunofabrizio15@gmail.com>
2021-03-17 19:57:51 -0300

    update

--- Introduccion/src/com/company/Ejercicio2.java
+++ Introduccion/src/com/company/Ejercicio2.java
@@ -4,7 +4,13 @@ public class Ejercicio2 {
 
     public static void main(String[] args) {
 
+        int X=2;
+        int Y=5;
 
+        double N=6.5;
+        double M=8.9;
+
+        System.out.println("X+Y ="+ (X+Y));
 
     }
 }
--- Introduccion/src/com/company/Ejercicio3.java
+++ Introduccion/src/com/company/Ejercicio3.java
@@ -4,5 +4,14 @@ public class Ejercicio3 {
 
     public static void main(String[] args) {
 
+        int N=5;
+
+        N+=77;
+        System.out.println("N = " + N);
+        N=3;
+        System.out.println("N = " + N);
+        N*=2;
+        System.out.println("N = " + N);
+        
     }
 }

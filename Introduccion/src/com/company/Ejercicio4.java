3d213aac079f07de17eb4d70bf35b4b587ffa0b7
brun0fz <brunofabrizio15@gmail.com>
2021-03-17 22:51:11 -0300

    update

--- Introduccion/src/com/company/Ejercicio4.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Ejercicio4 {
-
-    public static void main(String[] args) {
-
-
-    }
-}
--- Introduccion/src/com/company/Main.java
+++ Introduccion/src/com/company/Main.java
@@ -1,8 +1,23 @@
+//Ejercicio 01
+
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Ejercicio01\n");
+        ejercicio01();
+
+        System.out.println("Ejercicio02\n");
+        ejercicio02();
+
+        System.out.println("Ejercicio03\n");
+        ejercicio03();
+
+    }
+
+    public static void ejercicio01() {
 
         int N=5;
         double A=3.5;
@@ -15,7 +30,35 @@ public class Main {
 
         int valorNumerico = Character.getNumericValue(C);
 
+
         System.out.println("El valor numerico de C es: "+valorNumerico);
 
     }
+
+    public static void ejercicio02() {
+        int X=2;
+        int Y=5;
+
+        double N=6.5;
+        double M=8.9;
+
+        System.out.println("X+Y ="+ (X+Y));
+    }
+
+
+
+        public static void ejercicio03() {
+
+            int N=5;
+
+            N+=77;
+            System.out.println("N = " + N);
+            N=3;
+            System.out.println("N = " + N);
+            N*=2;
+            System.out.println("N = " + N);
+
+        }
+
+
 }

3ac612bcbe564e0cb7de14a91cc9a06b04e968b8
brun0fz <brunofabrizio15@gmail.com>
2021-03-21 23:05:11 -0300

    Update from git

--- Guia01/src/com/company/test/MainEj02.java
+++ Guia01/src/com/company/test/MainEj02.java
@@ -5,7 +5,7 @@ import com.company.clases.Empleado;
 public class MainEj02 {
 
     public static void main(String[] args) {
-        ///Ejercicio 02
+        ///Ejercicio 02..
 
         Empleado empleCarlos = new Empleado(23456345, "Carlos", "Gutierrez", 25000);
         Empleado empleAna = new Empleado(34234123, "Ana", "Sanchez", 27500);

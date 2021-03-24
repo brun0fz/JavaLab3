05e09703e7525653882fe837a942b1c90dbf347f
Bruno Fabrizio <brunofz@Brunos-MacBook-Air.local>
2021-03-24 03:16:50 -0300

    test

--- Guia01/src/com/company/test/MainEj01.java
+++ Guia01/src/com/company/test/MainEj01.java
@@ -5,6 +5,8 @@ import com.company.clases.Rectangulo;
 public class MainEj01 {
     public static void main(String[] args) {
 
+        ///test
+
         Rectangulo rectangulito = new Rectangulo(3,6);
 
         System.out.println("Alto: " + rectangulito.getAlto() + " Ancho: " + rectangulito.getAncho());
